42ccca2fa64789004d36d40324cd6d3813e8fff5
maningXu <18852676059@163.com>
2021-03-18 22:51:42 +0800

    init explore file

--- src/views/explore/index.vue
+++ src/views/explore/index.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="explore-homepage">
     <explore-special />
+    <round-table />
+    <collection />
+    <column />
+    <explore-footer />
   </div>
 </template>
 
 <script>
-import Special from './components/special'
+import Special from './components/special.vue'
+import RoundTable from './components/round-table.vue'
+import Collection from './components/collection.vue'
+import Column from './components/column.vue'
+import ExploreFooter from './components/explore-footer.vue'
 export default {
   data: function() {
     return {}
@@ -14,7 +22,11 @@ export default {
     document.title = '发现 - 知乎'
   },
   components: {
-    'explore-special': Special
+    'explore-special': Special,
+    'round-table': RoundTable,
+    collection: Collection,
+    column: Column,
+    'explore-footer': ExploreFooter
   }
 }
 </script>
