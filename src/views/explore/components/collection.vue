f2753f8b294f9ca0ed3848efcb174dab92d7b578
maningXu <18852676059@163.com>
2021-03-22 22:40:21 +0800

    init collection/column header&morebutton

--- src/views/explore/components/collection.vue
+++ src/views/explore/components/collection.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>collection</div>
+  <div class="collection">
+    <div class="content-section-header">
+      <svg fill="currentColor" viewBox="0 0 24 24" width="36" height="36">
+        <path
+          d="M5.515 19.64l.918-5.355-3.89-3.792c-.926-.902-.639-1.784.64-1.97L8.56 7.74l2.404-4.871c.572-1.16 1.5-1.16 2.072 0L15.44 7.74l5.377.782c1.28.186 1.566 1.068.64 1.97l-3.89 3.793.918 5.354c.219 1.274-.532 1.82-1.676 1.218L12 18.33l-4.808 2.528c-1.145.602-1.896.056-1.677-1.218z"
+          fill-rule="evenodd"
+        ></path>
+      </svg>
+      <span>热门收藏夹</span>
+    </div>
+    <div class="content-section-body">
+      <div class="collections"></div>
+    </div>
+    <div class="content-section-moreButton">
+      <a>
+        查看更多收藏夹
+        <svg fill="currentColor" viewBox="0 0 24 24" width="25" height="25">
+          <path
+            d="M9.218 16.78a.737.737 0 0 0 1.052 0l4.512-4.249a.758.758 0 0 0 0-1.063L10.27 7.22a.737.737 0 0 0-1.052 0 .759.759 0 0 0-.001 1.063L13 12l-3.782 3.716a.758.758 0 0 0 0 1.063z"
+            fill-rule="evenodd"
+          ></path>
+        </svg>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/explore/components/column.vue
+++ src/views/explore/components/column.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>column</div>
+  <div class="collection">
+    <div class="content-section-header">
+      <svg class="Zi Zi--EditCircle" fill="currentColor" viewBox="0 0 24 24" width="36" height="36">
+        <path
+          d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm4.043-15.524a.745.745 0 0 0-1.053.017l-6.857 7.071 2.237 2.17 6.857-7.071a.743.743 0 0 0-.016-1.052l-1.168-1.135zm-9.028 9.476l-.348 1.381 1.37-.39 1.274-.36-1.973-1.916-.323 1.285z"
+          fill-rule="evenodd"
+        ></path>
+      </svg>
+      <span>专栏</span>
+    </div>
+    <div class="content-section-body">
+      <div class="columns"></div>
+    </div>
+    <div class="content-section-moreButton">
+      <a>
+        查看更多专栏
+        <svg fill="currentColor" viewBox="0 0 24 24" width="25" height="25">
+          <path
+            d="M9.218 16.78a.737.737 0 0 0 1.052 0l4.512-4.249a.758.758 0 0 0 0-1.063L10.27 7.22a.737.737 0 0 0-1.052 0 .759.759 0 0 0-.001 1.063L13 12l-3.782 3.716a.758.758 0 0 0 0 1.063z"
+            fill-rule="evenodd"
+          ></path>
+        </svg>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
