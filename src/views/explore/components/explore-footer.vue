aad220ae7cea49249ff545b998d6d4fa41b81393
maningXu <18852676059@163.com>
2021-03-25 22:44:19 +0800

    complete explore footer

--- src/views/explore/components/explore-footer.vue
+++ src/views/explore/components/explore-footer.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>footer</div>
+  <footer class="footer">
+    <a href="https://liukanshan.zhihu.com">刘看山</a>
+    <a href="https://www.zhihu.com/question/19581624">知乎指南</a>
+    <a href="https://www.zhihu.com/term/zhihu-terms">知乎协议</a>
+    <a href="https://www.zhihu.com/app">应用</a>
+    <a href="https://app.mokahr.com/apply/zhihu/3819">工作</a>
+    <a href="https://www.zhihu.com/contact">联系我们</a>
+    <span>© 2021 知乎</span>
+  </footer>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style scoped></style>
+<style scoped>
+.footer {
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  height: 24px;
+  font-size: 13px;
+  color: #8590a6;
+  margin-top: 64px;
+}
+.footer a + a:before {
+  content: '\B7';
+  margin: 0 4px;
+}
+.footer span {
+  margin-left: 4px;
+}
+</style>
