49b053ecaf0c4be9a2fde632102aed82cf1d3fd1
maningXu <18852676059@163.com>
2021-03-31 21:37:19 +0800

    app content layout

--- src/views/vip/components/app-content.vue
+++ src/views/vip/components/app-content.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>app</div>
+  <div class="app-content">
+    <div class="app-commodity">
+      <div class="rankingList-root"></div>
+    </div>
+    <div class="app-myself"></div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {}
 </script>
 
 <style scoped>
-
+.app-content {
+  margin: 0 auto;
+  padding-top: 30px;
+  width: 1130px;
+  overflow: hidden;
+}
+.app-commidity {
+  float: left;
+  width: 829px;
+}
+.rankingList-root {
+  position: relative;
+  width: 100%;
+}
+.app-myself {
+  float: right;
+  width: 280px;
+}
 </style>
