c7a9bd7be91070a6baae5ad1a88b605aa1a21373
maningXu <18852676059@163.com>
2021-04-11 21:43:03 +0800

    complete vip-banner

---
+++ src/views/vip/components/vip-banner.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="vip-banner">
+    <div class="vip-banner-item active">
+      <div class="image-imageWrapper">
+        <div style="width:1920;padding-top:26.041666666666668%"></div>
+        <img
+          class="image-image"
+          src="https://pic1.zhimg.com/50/v2-d741f4406f843b0eb882e6e0ab534541.webp"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.vip-banner {
+  position: relative;
+  height: calc(100vw * 500 / 1920);
+}
+.vip-banner-item {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+}
+.vip-banner-item.active {
+  opacity: 1;
+  z-index: 1;
+  -webkit-animation: CarouselBanner-turnRight-ac9W5 0.3s ease-in-out;
+  animation: CarouselBanner-turnRight-ac9W5 0.3s ease-in-out;
+}
+.image-imageWrapper {
+  position: relative;
+  margin: 0 auto;
+  cursor: pointer;
+  -webkit-transition: width 0.6s ease, height 0.6s ease;
+  transition: width 0.6s ease, height 0.6s ease;
+  width: 100%;
+  height: calc(100vw * 500 / 1920);
+  flex-shrink: 0;
+}
+.image-image {
+  font-size: 12px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  object-fit: cover;
+  text-indent: -9999px;
+  border-radius: inherit;
+}
+</style>
--- src/views/vip/index.vue
+++ src/views/vip/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="vip">
+    <vip-banner />
     <menu-bar />
     <app-content />
   </div>
 </template>
 
 <script>
+import VipBanner from './components/vip-banner.vue'
 import MenuBar from './components/menu-bar.vue'
 import AppContent from './components/app-content.vue'
 
 export default {
   components: {
+    'vip-banner': VipBanner,
     'menu-bar': MenuBar,
     'app-content': AppContent
   },
