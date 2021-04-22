85992235ec66dbedf24cbd0b963d053c0a39fc73
maningXu <18852676059@163.com>
2021-04-22 21:18:22 +0800

    add account in form

--- src/views/login/login-form.vue
+++ src/views/login/login-form.vue
@@ -3,7 +3,74 @@
     <div class="sign-container-content"></div>
     <div class="split"></div>
     <div class="login-socialLogin"></div>
-    <div class="account"></div>
+    <div class="account">
+      <div class="account-opened">
+        <a>
+          <span style="display: inline-flex; align-items: center;">
+            &ZeroWidthSpace;
+            <svg
+              class="Zi Zi--BadgeCert css-vfpo4o"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              width="26"
+              height="26"
+            >
+              <g fill="none" fill-rule="evenodd">
+                <path
+                  fill="#0F88EB"
+                  d="M2.64 13.39c1.068.895 1.808 2.733 1.66 4.113l.022-.196c-.147 1.384.856 2.4 2.24 2.278l-.198.016c1.387-.122 3.21.655 4.083 1.734l-.125-.154c.876 1.084 2.304 1.092 3.195.027l-.127.152c.895-1.068 2.733-1.808 4.113-1.66l-.198-.022c1.386.147 2.402-.856 2.279-2.238l.017.197c-.122-1.388.655-3.212 1.734-4.084l-.154.125c1.083-.876 1.092-2.304.027-3.195l.152.127c-1.068-.895-1.808-2.732-1.66-4.113l-.022.198c.147-1.386-.856-2.4-2.24-2.279l.198-.017c-1.387.123-3.21-.654-4.083-1.733l.125.153c-.876-1.083-2.304-1.092-3.195-.027l.127-.152c-.895 1.068-2.733 1.808-4.113 1.662l.198.02c-1.386-.147-2.4.857-2.279 2.24L4.4 6.363c.122 1.387-.655 3.21-1.734 4.084l.154-.126c-1.083.878-1.092 2.304-.027 3.195l-.152-.127z"
+                ></path>
+                <path
+                  fill="#FFF"
+                  d="M9.78 15.728l-2.633-2.999s-.458-.705.242-1.362c.7-.657 1.328-.219 1.328-.219l1.953 2.132 4.696-4.931s.663-.348 1.299.198c.636.545.27 1.382.27 1.382s-3.466 3.858-5.376 5.782c-.98.93-1.778.017-1.778.017z"
+                ></path>
+              </g>
+            </svg>
+          </span>
+          开通机构号
+        </a>
+      </div>
+      <div class="account-split-line"></div>
+      <div class="account-download">
+        <button>
+          <span style="display: inline-flex; align-items: center;">
+            &ZeroWidthSpace;
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <path
+                fill="url(#paint0_linear)"
+                d="M16.131 0H3.87A3.869 3.869 0 000 3.869V16.13A3.869 3.869 0 003.869 20H16.13A3.869 3.869 0 0020 16.131V3.87A3.869 3.869 0 0016.131 0z"
+              ></path>
+              <path
+                fill="#fff"
+                d="M14.9 10.046V7.391l2.67-.137.232-.015c.147-.26.214-.559.192-.857-.02-.071-.059-.19-.178-.163a9.048 9.048 0 01-1.204.207c-.777.092-1.119.116-2.196.193-1.893.14-3.56.205-3.56.205a.722.722 0 00.75.724c.642-.027 1.582-.071 2.531-.119v2.617h-3.616a.753.753 0 00.753.754h2.843v2.008c0 .255-.13.341-.345.347h-1.1a1.187 1.187 0 00.632.742c.244.081.503.109.76.08.385-.02.836-.237.836-1.014V10.8h3.175a.362.362 0 00.361-.362v-.392H14.9z"
+              ></path>
+              <path
+                fill="#fff"
+                d="M12.414 8.251a.65.65 0 00-.908-.148l-.107.077 1.187 1.662.635-.457-.807-1.134zm4.147-.057l-.89 1.166.647.472 1.273-1.685-.107-.077a.653.653 0 00-.923.124zM9.737 7.031H6.263v6.065h.858l.112.732 1.154-.732h1.356L9.737 7.03zm-1.465 5.311l-.674.415-.065-.427h-.487V7.785h1.908v4.557h-.682zM5.637 9.761H4.379V7.785h1.436v-.368a.386.386 0 00-.383-.385h-2.24a115.436 115.436 0 01.347-1.018 1.187 1.187 0 00-.68.199.908.908 0 00-.385.551c-.151.416-.296.813-.368 1.05-.136.38-.466 1.048-.466 1.048.248-.002.493-.06.716-.169a1.205 1.205 0 00.575-.822c0-.027 0-.053.024-.086h.65V9.35c0 .142 0 .279-.018.412H2.433a.754.754 0 00-.754.754h1.837a4.955 4.955 0 01-.226 1.035 4.746 4.746 0 01-.845 1.484 9.363 9.363 0 01-.89.845 1.56 1.56 0 001.527-.27 4.21 4.21 0 00.908-1.623c.152-.483.258-.98.318-1.483h1.685v-.38a.362.362 0 00-.356-.362z"
+              ></path>
+              <path
+                fill="#fff"
+                d="M4.747 11.318l-.658.425 1.418 2.118a1.96 1.96 0 00.19-.629.908.908 0 00-.08-.617l-.87-1.297z"
+              ></path>
+              <defs>
+                <linearGradient
+                  id="paint0_linear"
+                  x1="-6.708"
+                  x2="25.504"
+                  y1="29.91"
+                  y2="-8.476"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop offset=".42" stop-color="#06F"></stop>
+                  <stop offset=".92" stop-color="#00C4FF"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+          下载知乎 App
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -67,5 +134,67 @@ export default {}
   align-items: center;
   background-color: #f6f6f6;
   display: -webkit-flex;
+}
+.account-opened {
+  box-sizing: border-box;
+  margin: 0;
+  min-width: 0;
+  flex: 1;
+}
+.account-opened > a {
+  box-sizing: border-box;
+  margin: 0;
+  min-width: 0;
+  color: inherit;
+}
+.account-opened > a svg {
+  margin-right: 0.5em;
+}
+.account-split-line {
+  box-sizing: border-box;
+  margin: 0;
+  min-width: 0;
+  height: 20px;
+  width: 1px;
+  background-color: #ebebeb;
+}
+.account-download {
+  box-sizing: border-box;
+  margin: 0;
+  min-width: 0;
+  flex: 1;
+  text-align: right;
+}
+.account-download button {
+  box-sizing: border-box;
+  margin: 0;
+  min-width: 0;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  display: inline-block;
+  text-align: center;
+  line-height: inherit;
+  -webkit-text-decoration: none;
+  text-decoration: none;
+  font-size: inherit;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  color: white;
+  background-color: #0066ff;
+  border: 0;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: 600;
+  color: #ffffff;
+  background-color: #0066ff;
+  border-radius: 4px;
+  background-color: unset;
+  color: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+  padding: 0;
 }
 </style>
